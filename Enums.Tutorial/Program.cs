2cd892bc3997c3b5bb2611cae1b21aac8a547ece
sshimabuku <sashi.shimabuku@gmail.com>
2021-03-05 11:39:14 -0500

    Create Enums.Tutorial project

---
+++ Enums.Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums.Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
