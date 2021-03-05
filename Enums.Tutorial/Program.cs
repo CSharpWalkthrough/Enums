f0518fa7a13fe609ac24a86365e821c209f5320b
sshimabuku <sashi.shimabuku@gmail.com>
2021-03-05 16:41:37 -0500

    Implement Enums tutorial

--- Enums.Tutorial/Program.cs
+++ Enums.Tutorial/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Enums.Tutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Enums.Tutorial
     {
         static void Main(string[] args)
         {
+            //Assigning an enum to a variable:
+            Season seasonSpring = Season.Spring;
+
+            //Get the enum as a string:
+            string autumn = Season.Autumn.ToString();
+            Console.WriteLine(autumn);
+
+            // or
+
+            Console.WriteLine(seasonSpring);
+            Console.WriteLine(Season.Summer);
+
+            //The following will NOT work tho:
+            //string summer = Season.Summer;
+
+            Console.WriteLine();
+
+            //Get the enum as an integer:
+            int summerCode = (int)Season.Summer; // 1
+            Console.WriteLine(summerCode);
+
+            Console.WriteLine();
+
+            //Get an integer as an enum:
+            int winterCode = 3;
+            Season seasonWinter = (Season)winterCode; //or (Season)3
+            Console.WriteLine(seasonWinter);
+
+            Console.ReadKey();
         }
     }
 }
